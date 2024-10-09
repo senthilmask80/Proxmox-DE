7361a85fa0c4c247920d2602689a98cbd90fc1ff
Senthil Kumaran <53327340+senthilmask80@users.noreply.github.com>
2024-10-09 23:44:33 +0530

    Update custom_openbox.sh

--- install_scripts/custom_openbox.sh
+++ install_scripts/custom_openbox.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
 # Main list of packages
-packages=(
-	"openbox"
-	"tint2"
-    "terminator"
-    "firefox-esr"
-    "xfce4-appfinder"
-)
+packages=("openbox" "tint2" "terminator" "firefox-esr" "xfce4-appfinder" "xorg" "xinit" "dialog" "menu" "obconf" "xfce4-power-manager")
+x11_packages=("xserver-xorg" "xbacklight" "xbindkeys" "xvkbd" "xinput" "xdotool" "xcompmgr" "lxappearance" "lxappearance-obconf" "feh" "redshift")
+basic_packages=("build-essential" "aptitude" "apt-transport-https" "software-properties-common" "gnupg" "ca-certificates")
+dbus_packages=("policykit-1-gnome" "network-manager" "network-manager-gnome" "avahi-daemon" "acpi" "acpid" "gvfs-backends" "libgtk-3-dev" "connman" "connman-gtk" "binutils")
+zip_packages=("unzip" "p7zip" "zip")
+utils_packages=("thunar" "thunar-archive-plugin" "thunar-volman" "mtools" "dosfstools" "rofi" "dunst" "libnotify-bin" "htop" "xscreensaver-gl-extra" "xscreensaver-data-extra" "autoconf" "dnsutils")
+extra_packages=("file-roller" "exa" "scrot" "libnotify-dev" "micro" "xclip" "nitrogen" "simplescreenrecorder" "ristretto" "gmrun" "numlockx" "conky" "slick-greeter" "cairo-dock" "dbus-x11" "hsetroot" "qt5-style-plugins")
+audio_packages=("pulseaudio" "pavucontrol" "volumeicon-alsa" "pamixer" "pulsemixer")
+themes_packages=("papirus-icon-theme" "arc-theme" "moka-icon-theme")
+geany_packages=("geany" "geany-plugin-addons" "geany-plugin-git-changebar" "geany-plugin-spellcheck" "geany-plugin-treebrowser" "geany-plugin-markdown" "geany-plugin-insertnum" "geany-plugin-lineoperations" "geany-plugin-automark")
+fonts_packages=("fonts-recommended" "fonts-font-awesome" "fonts-terminus" "fonts-noto")
 
-# Function to read common packages from a file
-read_common_packages() {
-    local common_file="$1"
-    if [ -f "$common_file" ]; then
-        packages+=( $(< "$common_file") )
-    else
-        echo "Common packages file not found: $common_file"
-        exit 1
-    fi
-}
-
-# Read common packages from file
-read_common_packages /tmp/Proxmox-DE/install_scripts/common_packages.txt
 
 # Function to install packages if they are not already installed
 install_packages() {
@@ -50,25 +41,53 @@ install_packages() {
 }
 
 # Call function to install packages
-install_packages "${packages[@]}"
+install_packages "${packages[@]}";
+echo "The default packages is installation"
+install_packages "${x11_packages[@]}";
+echo "The x11 packages is installation"
+install_packages "${basic_packages[@]}";
+echo "The basic packages is installation"
+install_packages "${dbus_packages[@]}";
+echo "The dbus-x11 packages is installation"
+install_packages "${zip_packages[@]}";
+echo "The zip packages is installation"
+install_packages "${utils_packages[@]}";
+echo "The utils packages is installation"
+install_packages "${extra_packages[@]}";
+echo "The extra packages is installation"
+install_packages "${audio_packages[@]}";
+echo "The audio packages is installation"
+install_packages "${themes_packages[@]}";
+echo "The themes packages is installation"
+install_packages "${geany_packages[@]}";
+echo "The geany packages is installation"
+install_packages "${fonts_packages[@]}";
+echo "The fonts packages is installation"
 
 sudo systemctl enable avahi-daemon
 sudo systemctl enable acpid
 
 xdg-user-dirs-update
+
 mkdir -p ~/Screenshots/
 mkdir -p ~/.themes/
 
 SCRIPT_DIR=/tmp/Proxmox-DE
 
+echo "The obmenu packages is installation"
 bash /tmp/Proxmox-DE/install_scripts/obmenu.sh
 
+echo "The vue-cli packages is installation"
 bash /tmp/Proxmox-DE/install_packages/vue-cli.sh
 
+echo "The webkit-lightdm packages is installation"
 bash /tmp/Proxmox-DE/webkit-lightdm/Saluto/install.sh
 
 # check FT-Labs picom and nerdfonts are installed
+echo "The picom packages is installation"
 bash /tmp/Proxmox-DE/install_scripts/picom.sh
+
+echo "The nerdfonts packages is installation"
 bash /tmp/Proxmox-DE/install_scripts/nerdfonts.sh
 
 # adding gtk theme and icon theme
@@ -77,9 +96,9 @@ mv ~/.config/openbox/Simply_Circles_Dark ~/.themes
 git clone https://github.com/addy-dclxvi/openbox-theme-collections ~/.themes
 
 # moving custom config
-\cp -r /tmp/Proxmox-DE/Prox-Dots/.config/openbox/ ~/.config/
-\cp -r /tmp/Proxmox-DE/Prox-Dots/.config/tint2/ ~/.config/
-\cp -r /tmp/Proxmox-DE/Prox-Dots/.config/dunst/ ~/.config/
-\cp -r /tmp/Proxmox-DE/Prox-Dots/.config/picom/ ~/.config/
-\cp -r /tmp/Proxmox-DE/Prox-Dots/.config/kitty/ ~/.config/
-\cp -r /tmp/Proxmox-DE/Prox-Dots/.config/backgrounds/ ~/.config/
+\ncp -r /tmp/Proxmox-DE/Prox-Dots/.config/openbox/ ~/.config/
+\ncp -r /tmp/Proxmox-DE/Prox-Dots/.config/tint2/ ~/.config/
+\ncp -r /tmp/Proxmox-DE/Prox-Dots/.config/dunst/ ~/.config/
+\ncp -r /tmp/Proxmox-DE/Prox-Dots/.config/picom/ ~/.config/
+\ncp -r /tmp/Proxmox-DE/Prox-Dots/.config/kitty/ ~/.config/
+\ncp -r /tmp/Proxmox-DE/Prox-Dots/.config/backgrounds/ ~/.config/
