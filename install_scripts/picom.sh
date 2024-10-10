69f7db222e31a612e3588f3b962b30303cc4aecf
Senthil Kumaran <53327340+senthilmask80@users.noreply.github.com>
2024-10-10 08:39:22 +0530

    Update picom.sh

--- install_scripts/picom.sh
+++ install_scripts/picom.sh
@@ -18,8 +18,8 @@ echo "Picom is not installed. Installing..."
 
 sudo apt install -y libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev
 
-git clone https://github.com/FT-Labs/picom ~/bookworm-scripts/picom
-cd ~/bookworm-scripts/picom
+git clone https://github.com/FT-Labs/picom /tmp/Proxmox-DE/picom
+cd /tmp/Proxmox-DE/picom
 meson setup --buildtype=release build
 ninja -C build
 sudo ninja -C build install
