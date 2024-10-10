25f4433bd9e047464e1122c14ba3cff0f2222ca9
Senthil Kumaran <53327340+senthilmask80@users.noreply.github.com>
2024-10-10 09:09:30 +0530

    Update vue-cli.sh

--- install_scripts/vue-cli.sh
+++ install_scripts/vue-cli.sh
@@ -5,6 +5,8 @@ sudo apt install libssl-dev
 # you already have what is necessary on your system to install NVM. To do this, perform the following step:
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
+source ~/.bashrc
+
 # I chose the latest LTS version, by the time I'm writing this * Gist *, it's v8.9.1. You can install it by typing:
 nvm install v8.9.1
 
@@ -12,5 +14,6 @@ nvm install v8.9.1
 nvm alias default 8.9.1
 
 # Installing the vue-cli
-npm install -g vue-cli --force
+npm install -g vue-cli
 
+npm install -g vue-cli --force
